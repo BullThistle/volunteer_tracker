28d47cc51a9b0aac67fad0b1a56b9a4f26007b38
Rafael Furry <rfurry@gmail.com>
2017-09-22 10:42:10 -0700

    create projects class

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
+  end
+end
+
+def save
+  result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+  @id = result.first.fetch('id').to_i
+end
+def method_name
+
+end
