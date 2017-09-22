e7357719f7194f7ddab6302ca78e4f670c2fe710
Rafael Furry <rfurry@gmail.com>
2017-09-22 13:49:22 -0700

    add class methods to project

--- lib/project.rb
+++ lib/project.rb
@@ -4,12 +4,31 @@ class Project
     @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
   end
-end
 
-def save
-  result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
-  @id = result.first.fetch('id').to_i
-end
-def method_name
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first.fetch('id').to_i
+  end
 
+  def ==(other_project)
+    @title == other_project.title
+  end
+
+  def self.all
+    projects_out = DB.exec("SELECT * FROM projects;")
+    projects_out.map do |project|
+      Project.new({
+        title: project['title'],
+        id: project['id'].to_i
+        })
+    end
+  end
+
+  def self.find(id)
+    Project.all.each do |project|
+      if project.id == id
+        return id
+      end
+    end
+  end
 end
