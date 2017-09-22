a7b7632465c87621e0f10e8c0958108a41f43bb1
Rafael Furry <rfurry@gmail.com>
2017-09-22 16:46:39 -0700

    work on app.rb

--- app.rb
+++ app.rb
@@ -2,9 +2,20 @@ require 'sinatra'
 require 'sinatra/reloader'
 also_reload 'lib/**/*.rb'
 require './lib/project'
+require './lib/volunteer'
 require 'pry'
 require 'pg'
 
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
 get '/' do
+  @projects = Project.all
   erb:main
+end
+
+post '/' do
+  title = params.fetch('title')
+  project = Project.new({title: title, id: nil})
+  project.save
+  @projects = Project.all
 end
